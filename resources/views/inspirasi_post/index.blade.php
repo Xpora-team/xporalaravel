e61508f3b8b3b676bfc78f58e01fa74b3c0b5691
[Dinda] <adiantidinda@gmail.com>
2022-06-24 14:20:40 +0700

    [UPDATE] table inspirasi post

--- resources/views/inspirasi_post/index.blade.php
+++ resources/views/inspirasi_post/index.blade.php
@@ -1,48 +1,47 @@
 @extends('layouts.admin')
 @section('main-content')
-<link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css">
 
-<div class="card shadow mb-4">
-    <div class="card-body">
-<div class="table-responsive">
-<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-    <h1>Tabel Inspirasi Post</h1>
-    <tr>
-        <th>ID_Inspirasi</th>
-        <th>Posting Name</th>
-        <th>Category</th>
-        <th>Level</th>
-        <th>Number of view</th>
-        <th>Number of application</th>
-        <th>status</th>
-        <th>upload_date</th>
-    </tr>
-    @foreach ($inspirasi_post as $w)
-        <tr onclick="sData(this)">
-        <td>{{$w->ID_Inspirasi}}</td>
-        <td>{{$w->Nama_Kelas}}</td>
-        <td>{{$w->Category}}</td>
-        <td>{{$w->Level}}</td>
-        <td>{{$w->view}}</td>
-        <td>{{$w->application}}</td>
-        <td>{{$w->status}}</td>
-        <td>{{$w->upload_date}}</td>
-        </tr>
-        </thead>
-    @endforeach
-    </table>
+    <div class="card shadow mb-4">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <h1>Tabel Inspirasi Post</h1>
+                    <tr>
+                        <th>ID_Inspirasi</th>
+                        <th>Posting Name</th>
+                        <th>Category</th>
+                        <th>Level</th>
+                        <th>Number of view</th>
+                        <th>Number of application</th>
+                        <th>status</th>
+                        <th>upload_date</th>
+                    </tr>
+                    @foreach ($inspirasi_post as $w)
+                        <tr onclick="sData(this)">
+                            <td>{{ $w->ID_Inspirasi }}</td>
+                            <td>{{ $w->Nama_Kelas }}</td>
+                            <td>{{ $w->Category }}</td>
+                            <td>{{ $w->Level }}</td>
+                            <td>{{ $w->view }}</td>
+                            <td>{{ $w->application }}</td>
+                            <td>{{ $w->status }}</td>
+                            <td>{{ $w->upload_date }}</td>
+                        </tr>
+                        </thead>
+                    @endforeach
+                </table>
+            </div>
+        </div>
     </div>
-    </div>
-    </div>
-    @endsection
-    @push('scripts')
+@endsection
+@push('scripts')
     <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>
-<script>
-    $(document).ready( function () {
-        $('#myTable').DataTable();
-    } );
-        </script>
-    
-    @endpush
+    <script>
+        $(document).ready(function() {
+            $('#myTable').DataTable();
+        });
+    </script>
+@endpush
--- resources/views/inspirasi_user/index.blade.php
+++ resources/views/inspirasi_user/index.blade.php
@@ -1,72 +1,77 @@
 @extends('layouts.admin')
 @section('main-content')
-<link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css">
-<link rel="stylesheet" href="{{ asset('js/script.js') }}">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css">
 
-<div class="card shadow mb-4">
-    <div class="card-header py-3">
-    </div>
-<div class="card-body">
-<div class="table-responsive">
-<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-    <thead>
-        <tr>
-        <th rowspan="2" class="align-middle">#</th>
-            <th>User ID</th>
-            <th>User Name</th>
-            <th>Level</th>
-            <th>Produk BNI</th>
-            <th>Number of view</th>
-            <th>Number of application</th>
-            <th>Verification</th>
-            <th>Last Access</th>
-            <th>Action</th>
-        </tr>
-        <tr>
-                            <th class="searchHeader">Search</th>
-                            <th class="searchHeader">Search</th>
-                            <th class="searchHeader">Search</th>
-                            <th class="searchHeader">Search</th>
-                            <th class="searchHeader">Search</th>
-                            <th class="searchHeader">Search</th>
-                            <th class="searchHeader">Verification</th>
-                            <th class="searchHeader">Create Date</th>
+    <div class="card shadow mb-4">
+        <div class="card-body">
+            <div class="table-responsive">
+                <h1>Tabel Inspirasi User</h1>
+                <table class="table table-bordered" id="myTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>ID Produk</th>
+                            <th>User ID</th>
+                            <th>Produk</th>
+                            <th>Min Pengajuan</th>
+                            <th>Realisasi Dana</th>
+                            <th>Status</th>
+                            <th>Create Date</th>
+
                         </tr>
-    </thead>
-    <tbody>
+                    </thead>
+                    <tbody>
+                        @foreach ($data as $item)
+                            <tr onclick="sData(this)">
 
-        @foreach ($data as $item)
-        <tr onclick="sData(this)">
-            <tr onclick="sData(this)">
-            <td style="text-align:right; white-space:nowrap;"><input class="form-check-input" type="checkbox" onclick="csData(this)" value="option1"></td>
-            <td>{{ $item['user_id'] }}</td>
-            <td>{{ $item['user_name'] }}</td>
-            <td>{{ $item['level'] }}</td>
-            <td>{{ $item['produkbni'] }}</td>
-            <td>{{ $item['view'] }}</td>
-            <td>{{ $item['application'] }}</td>
-            <td>{{ $item['verification'] }}</td>
-            <td>{{ $item['date'] }}</td>
-            <td style="text-align:center; white-space:nowrap;"><a href="#" class="btn btn-info btn-sm">Detail</a></td>
-            </tr>
-        </tr>
-    </tbody>
-        </thead>
-    @endforeach
-    </table>
+                                <td>{{ $item['user_id'] }}</td>
+                                <td>{{ $item['user_name'] }}</td>
+                                <td>{{ $item['level'] }}</td>
+                                <td>{{ $item['produkbni'] }}</td>
+                                <td>{{ $item['view'] }}</td>
+                                <td>{{ $item['application'] }}</td>
+                                <td>{{ $item['verification'] }}</td>
+                                <td>{{ $item['date'] }}</td>
+                                <td style="text-align:center; white-space:nowrap;"><a href="#"
+                                        class="btn btn-info btn-sm">Detail</a></td>
+                            </tr>
+                            </thead>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
-    </div>
-    @endsection
-    @push('scripts')
-    
-    <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>
-    <script src="{{ asset('js/script.js') }}"></script>
-<script>
-    $(document).ready( function () {
-        $('#myTable').DataTable();
-    } );
-        </script>
-    
-    @endpush
+@endsection
+@push('js')
+    <script>
+        $(document).ready(function() {
+            console.log('table')
+            window.setTimeout(
+                () => {
+                    $('#myTable').DataTable({
+                        dom: 'Bfrtip',
+                        buttons: [{
+                                extend: 'excel',
+                                text: 'Download Filter Data',
+                                filename: 'Filter Produk BNI',
+                                title: null
+                            },
+                            {
+                                extend: 'excel',
+                                text: 'Download All Data',
+                                exportOptions: {
+                                    modifier: {
+                                        selected: null
+                                    }
+                                },
+                                filename: 'Produk BNI',
+                                title: null
+                            }
+                        ],
+                        select: true
+                    });
+                }, 500
+            )
+        });
+    </script>
+@endpush
